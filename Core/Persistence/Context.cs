d48b1262fa9988821d35c9559f47dcd68cd0abe1
Michael Friis <friism@gmail.com>
2012-10-30 14:49:59 -0700

    remove property

--- Core/Persistence/Context.cs
+++ Core/Persistence/Context.cs
@@ -1,12 +1,9 @@
 ﻿using System.Data.Entity;
-using Core.Model;
 
 namespace Core.Persistence
 {
 	public class Context : DbContext
 	{
-		public DbSet<User> Users { get; set; }
-
 		protected override void OnModelCreating(DbModelBuilder modelBuilder)
 		{
 			Database.SetInitializer(new MigrateDatabaseToLatestVersion<Context, Configuration>());
