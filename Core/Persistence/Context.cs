8f952792613c4ebb6c8e5efcb0040683820cc5a5
Michael Friis <friism@gmail.com>
2012-10-30 14:56:51 -0700

    add necessary logic in Context

--- Core/Persistence/Context.cs
+++ Core/Persistence/Context.cs
@@ -1,4 +1,7 @@
-﻿using System.Data.Entity;
+﻿using Core.Model;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
 
 namespace Core.Persistence
 {
@@ -6,7 +9,23 @@ namespace Core.Persistence
 	{
 		protected override void OnModelCreating(DbModelBuilder modelBuilder)
 		{
+			ConfigureModel(modelBuilder);
+
 			Database.SetInitializer(new MigrateDatabaseToLatestVersion<Context, Configuration>());
+
+			base.OnModelCreating(modelBuilder);
+		}
+
+		private void ConfigureModel(DbModelBuilder modelBuilder)
+		{
+			var entityMethod = typeof(DbModelBuilder).GetMethod("Entity");
+
+			var entityTypes = Assembly.GetAssembly(typeof(Entity)).GetTypes()
+				.Where(x => x.IsSubclassOf(typeof(Entity)) && !x.IsAbstract);
+			foreach (var type in entityTypes)
+			{
+				entityMethod.MakeGenericMethod(type).Invoke(modelBuilder, new object[] { });
+			}
 		}
 	}
 }
