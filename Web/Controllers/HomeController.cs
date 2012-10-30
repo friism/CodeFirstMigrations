a1a2fbc88d05db9085754012438a8d26e103fa7c
Michael Friis <friism@gmail.com>
2012-10-30 14:27:54 -0700

    use repository in controller

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,22 +1,21 @@
-﻿using System.Web.Mvc;
-using Core.Model;
+﻿using Core.Model;
 using Core.Persistence;
+using System.Web.Mvc;
 
 namespace Web.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly Context _context = new Context();
+		private readonly IRepository _repository = new Repository(new Context());
 
 		public ActionResult Index()
 		{
-			return View(_context.Users);
+			return View(_repository.GetAll<User>());
 		}
 
 		public ActionResult Create(User user)
 		{
-			_context.Users.Add(user);
-			_context.SaveChanges();
+			_repository.SaveOrUpdate(user);
 
 			return RedirectToAction("Index");
 		}
