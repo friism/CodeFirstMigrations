5eb31a882c187dc206c10820f45c2b482b9b15e5
Michael Friis <friism@gmail.com>
2012-10-30 14:13:35 -0700

    introduce repository

---
+++ Core/Persistence/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Model;
+using System.Linq;
+
+namespace Core.Persistence
+{
+	public interface IRepository
+	{
+		T Get<T>(int id) where T : Entity;
+		IQueryable<T> GetAll<T>() where T : Entity;
+		void SaveOrUpdate<T>(T entity) where T : Entity;
+		void Delete<T>(T entity) where T : Entity;
+	}
+}
---
+++ Core/Persistence/Repository.cs
@@ -0,0 +1,65 @@
+﻿using Core.Model;
+using System;
+using System.Collections.Concurrent;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+
+namespace Core.Persistence
+{
+	public class Repository : IRepository, IDisposable
+	{
+		private Context _context;
+		private readonly ConcurrentDictionary<Type, object> _dbSets =
+			new ConcurrentDictionary<Type, object>();
+
+		public Repository(Context context)
+		{
+			_context = context;
+		}
+
+		public T Get<T>(int id) where T : Entity
+		{
+			return GetDbSet<T>().Find(id);
+		}
+
+		public IQueryable<T> GetAll<T>() where T : Entity
+		{
+			return GetDbSet<T>();
+		}
+
+		public void SaveOrUpdate<T>(T entity) where T : Entity
+		{
+			if (_context.Entry(entity).State == EntityState.Detached)
+			{
+				GetDbSet<T>().Add(entity);
+			}
+			else
+			{
+				_context.Entry(entity).State = EntityState.Modified;
+			}
+
+			_context.SaveChanges();
+		}
+
+		public void Delete<T>(T entity) where T : Entity
+		{
+			GetDbSet<T>().Remove(entity);
+			_context.SaveChanges();
+		}
+
+		private DbSet<T> GetDbSet<T>() where T : Entity
+		{
+			return (DbSet<T>)_dbSets.GetOrAdd(typeof(T), x => _context.Set<T>());
+		}
+
+		public void Dispose()
+		{
+			if (_context != null)
+			{
+				_context.Dispose();
+				_context = null;
+			}
+		}
+	}
+}
